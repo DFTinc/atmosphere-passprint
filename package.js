9e5470a003557cafab137c54897aa886d663acec
Matthew Wheatley <mwheatley@diamondfortress.com>
2018-09-06 09:48:15 -0500

    version 1.0.0 added package name for publishing

--- package.js
+++ package.js
@@ -1,7 +1,13 @@
 Package.describe({
-    summary: "PassPrint OAuth2 flow",
+    name: 'diamondfortress:passprint',
     version: "1.0.0",
-    git: "https://github.com/DFTinc/atmosphere-passprint.git"
+    // Brief, one-line summary of the package.
+    summary: "PassPrint OAuth2 flow",
+    // URL to the Git repository containing the source code for this package.
+    git: "https://github.com/DFTinc/atmosphere-passprint.git",
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
